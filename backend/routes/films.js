d3843af48e25b48bcba991b134c6524cc58c9c8d
Youcef <mtn.youssef.kh@gmail.com>
2024-03-10 02:19:43 +0100

    created the users api

---
+++ backend/routes/films.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+// router.post('/', creerFilm)
+// router.get('/:id', obtenirFilm)
+// router.get('/', obtenirFilms)
+// router.delete('/:id', supprimerFilm)
+// router.patch('/:id', majFilm)
+
+module.exports = router
